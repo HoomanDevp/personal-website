cb12335af1fbfcf1c66129f02c8291fc4d930d38
Hooman <67341187+HoomanDevp@users.noreply.github.com>
2026-08-11 23:58:30 +0330

    feat: simplify app shell for portfolio

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,6 @@
-import { HeaderResponsive } from "@/components/Header/HeaderResponsive";
-import {
-  ColorScheme,
-  ColorSchemeProvider,
-  MantineProvider,
-} from "@mantine/core";
-import { useLocalStorage } from "@mantine/hooks";
-import { AppProps } from "next/app";
-import "../components/MouseHighlight/style.css";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
-export default function App(props: AppProps) {
-  const { Component, pageProps } = props;
-  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
-    key: "mantine-color-scheme",
-    defaultValue: "dark",
-    getInitialValueInEffect: true,
-  });
-  const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
-
-  return (
-    <>
-      <ColorSchemeProvider
-        colorScheme={colorScheme}
-        toggleColorScheme={toggleColorScheme}
-      >
-        <MantineProvider
-          withGlobalStyles
-          withNormalizeCSS
-          theme={{
-            colorScheme: colorScheme,
-          }}
-        >
-          <HeaderResponsive
-            colorScheme={colorScheme}
-            toggleColorScheme={toggleColorScheme}
-          />
-
-          <Component {...pageProps} />
-        </MantineProvider>
-      </ColorSchemeProvider>
-    </>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
