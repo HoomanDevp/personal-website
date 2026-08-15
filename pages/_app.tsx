20f219e6345d7bf12f15469956e01895d0f77293
Hooman <67341187+HoomanDevp@users.noreply.github.com>
2026-08-15 19:14:27 +0330

    feat: expose resume download across portfolio

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from "next/app";
+import ResumeDownload from "../components/ResumeDownload";
 import "../styles/globals.css";
 import "../styles/profile-photo.css";
+import "../styles/resume-download.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ResumeDownload />
+    </>
+  );
 }
