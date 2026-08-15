cd399aa26df3caaf96a2cc44755a7e0f18eb5a04
Hooman <67341187+HoomanDevp@users.noreply.github.com>
2026-08-15 19:34:14 +0330

    fix: use root GitHub Pages metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import ResumeDownload from "../components/ResumeDownload";
 import "../styles/globals.css";
@@ -7,6 +8,11 @@ import "../styles/resume-download.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta property="og:url" content="https://hoomandevp.github.io/" key="og:url" />
+        <link rel="canonical" href="https://hoomandevp.github.io/" key="canonical" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" key="favicon" />
+      </Head>
       <Component {...pageProps} />
       <ResumeDownload />
     </>
